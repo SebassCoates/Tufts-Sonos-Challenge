1d2af45e2e25d9b176821927ab173280e51bad64
kedestin <kevin.destin@tufts.edu>
2017-10-07 16:35:10 -0400

    We got audio

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,24 @@
 function onLoad(){
-	navigator.getUserMedia  = navigator.getUserMedia ||
-                  navigator.webkitGetUserMedia ||
-                  navigator.mozGetUserMedia ||
-                  navigator.msGetUserMedia;
 
-        if (navigator.getUserMedia){
-        	alert("We're in");
-        	navigator.getUserMedia({audio: true});
-        }
-        else {
-        	alert("No audio capabilities");
-        }
+	audioChunks = [];
 
+	navigator.mediaDevices.getUserMedia({audio:true})
+	.then(stream => {
+		rec = new MediaRecorder(stream);
+		rec.ondataavailable = e => {
+		audioChunks.push(e.data);
+			if (rec.state == "inactive"){
+        			let blob = new Blob(audioChunks,{type:'audio/x-mpeg-3'});
+    			}
+		}
+	})
+	.catch(e=>console.log(e));
 
+	e => {rec.start();}
+	i = 0;
+	while ( i < 1000000 ){
+		i++;
+	}
+	e => {rec.stop();}
 
 }
