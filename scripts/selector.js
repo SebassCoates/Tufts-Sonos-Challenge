8c5c7c3477a64e3e54198e8d7266f4433b93ebe5
Sebastian Coates <sebass@resnet152-237.medford.tufts.edu>
2017-10-09 12:52:31 -0400

    updated uruls

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,5 +1,5 @@
 SONG_DICT = {};
-SONG_DICT["negative"] = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:3A37i9dQZF1DX3YSRoSdA634";
+SONG_DICT["negative"] = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX3YSRoSdA634";
 SONG_DICT["positive"] = "https://open.spotify.com/embed?uri=spotify:user:spotify:playlist:37i9dQZF1DX9XIFQuFvzM4";
 
 function pickSong(responseText){
