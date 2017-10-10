72b6e21d031c3e8e73be916f94cd903e6c64fdfa
Sebastian Coates <sebass@resnet152-237.medford.tufts.edu>
2017-10-09 20:59:55 -0400

    remove overlapping bg

--- scripts/main.js
+++ scripts/main.js
@@ -179,6 +179,7 @@ function pickSong(responseText){
         sentiment = json.documentSentiment.score;
         if (sentiment < 0) {
                 document.getElementById('player').src=SONG_DICT["negative"];
+                document.body.style.backgroundImage = none;
                 document.body.style.backgroundImage = "url('assets/sadbg.jpg')";
 
         } else {
